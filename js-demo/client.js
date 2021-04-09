2e641324a87db2af8892f350b78ca60b0b1ee536
Sarouar hossain <sarouarhossain@team-lab.com>
2021-04-09 17:39:24 +0900

    server client demo in js

---
+++ js-demo/client.js
@@ -0,0 +1,19 @@
+const grpc = require("grpc")
+const protoLoader = require("@grpc/proto-loader")
+const packageDef = protoLoader.loadSync("todo.proto", {})
+const grpcObject = grpc.loadPackageDefinition(packageDef)
+const todoPackage = grpcObject.todoPackage
+
+const client = new todoPackage.Todo("localhost:5555", grpc.credentials.createInsecure())
+
+const text = process.argv[2]
+
+client.createTodo({
+    "text" : text
+}, (err, res) => {
+    //console.log("Server response: " + JSON.stringify(res))
+})
+
+client.readTodos({}, (err,res) => {
+    console.log("Server response: " + JSON.stringify(res))
+})
---
+++ js-demo/server.js
@@ -0,0 +1,36 @@
+const grpc = require("grpc")
+const protoLoader = require("@grpc/proto-loader")
+const packageDef = protoLoader.loadSync("todo.proto", {})
+const grpcObject = grpc.loadPackageDefinition(packageDef)
+const todoPackage = grpcObject.todoPackage
+
+const server = new grpc.Server()
+server.bind("0.0.0.0:5555", grpc.ServerCredentials.createInsecure())
+
+server.addService(todoPackage.Todo.service, {
+    "createTodo": createTodo,
+    "readTodos": readTodos
+})
+
+server.start();
+
+const todos = []
+let id = 100
+function createTodo (call,callback) {
+    let todo = {
+        "id" : id++,
+        "text" : call.request.text
+    }
+    
+    todos.push(todo)
+    
+    console.log("Current State: "+JSON.stringify(todos))
+    callback(null,todo)
+}
+
+function readTodos (call,callback) {
+    callback(null, {
+        "items" : todos
+    })
+}
+
