8ba5172d2657507a94b2dbbe02c663cdb17e30de
sarouarhossain <sarouar.hossain10@outlook.com>
2021-04-11 19:27:26 +0900

    test1

---
+++ grpctest/src/main/java/com/demo/grpctest/GrpctestApplication.java
@@ -0,0 +1,13 @@
+package com.demo.grpctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpctestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpctestApplication.class, args);
+	}
+
+}
---
+++ grpctest/src/test/java/com/demo/grpctest/GrpctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.grpctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpctestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
